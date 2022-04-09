058cc846cb39c63d4e261eb9c9e13554de6e8fdb
Zhanananara <zhannara.nurlanova@gmail.com>
2022-04-09 18:41:18 +0600

    about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,100 @@
 import React from "react";
+import "./pages.css";
 
 const About = () => {
   return (
-    <div>
-      <h1>About page</h1>
+    <div style={{ textAlign: "center" }}>
+      <h1>THE HISTORY</h1>
+      <div className="image">
+        <img
+          width="60%"
+          src="https://mylitta.ru/uploads/posts/2011-07/1311836328_coco-chanel-1.jpg"
+          alt=""
+        />
+      </div>
+      <div className="post">
+        <h2>History of the Chanel brand</h2>
+        <p>
+          Chanel is one of the most influential French fashion houses, founded
+          by couturier Gabrielle "Coco" Chanel. The brand has established itself
+          well both in the field of Pret-a-porter and in the field of Haute
+          Couture, producing luxury goods: clothes, shoes, bags, perfumes,
+          cosmetics, jewelry. For February, 2013 the Fashion house is a part of
+          the Parisian Haute Couture Syndicate. The founder of the House got the
+          nickname "Coco" while working as a cabaret singer in France. This
+          pseudonym became her name in the fashion industry. Chanel's vision for
+          women's style was to replace glamorous and sexy pieces with more
+          casual and elegant pieces. So, the couturier brought into fashion
+          simple straight dresses without corsets with a minimum of decor, as
+          well as women's trouser suits. The most famous product of the designer
+          and her Fashion House today is the little black dress (“little black
+          dress”).
+        </p>
+      </div>
+      <div className="image">
+        <img
+          width="60%"
+          src="https://focus.ua/static/storage/thumbs/920x465/0/e9/54fed365-99753264fa9d25cf096d4b7b043fbe90.jpg?v=1122_1"
+          alt=""
+        />
+      </div>
+      <div className="post">
+        <h2>Chanel No. 5: the creation of a legendary fragrance</h2>
+        <p>
+          In 1921, Coco Chanel introduced the first women's perfume - Chanel No.
+          5 perfume. The history of the creation of this perfume is closely
+          connected with the relationship between Coco and the Great Russian
+          Prince Dmitry Pavlovich Romanov. Chanel and the prince met in Biarritz
+          in 1920 and spent the next year together. It was then that Dmitry
+          Pavlovich introduced his passion to the perfumer of the Romanov family
+          - Ernest Bo, who, at the request of the milliner, helped her in
+          creating her own perfume. As conceived by Coco, the fragrance was to
+          embody the smell of a woman entirely. In addition, she wanted the
+          composition to include more different essences, and not just one or
+          two, as in the perfumes of those times. Ernest Bo has been working on
+          perfumes for many months, mixing many components. At one of the
+          meetings with Coco, he showed her several variants of the fragrances
+          he created. Chanel chose the fifth bottle in a row, and, in addition,
+          Chanel's favorite number was also 5. The designer decided to focus on
+          this blend of essences and named her first Chanel No. 5 perfume. The
+          fragrance composition consisted of 80 ingredients, including
+          ylang-ylang from the Comoros, orange blossom, jasmine from the Grass
+          fields, May rose, sandalwood, Bourbon vetiver and aldehydes –
+          artificial components whose concentration in Chanel perfumes was a
+          record for those years. According to legend, when creating the
+          fragrance, Bo accidentally exceeded the dose of aldehydes in the
+          fragrance, but that's why Chanel liked the smell so much. And the
+          couturier was not mistaken in her choice, because the perfume became a
+          hit. In addition, Chanel No. 5 is still an ageless classic, a standard
+          of elegance and one of the most exquisite women's fragrances according
+          to perfumers Chanel No. 5 toilet perfumes based on the original ones
+          were created in 1986 by the perfumer of the Fashion House Jacques
+          Polge.
+        </p>
+      </div>
+      <div className="image">
+        <img
+          width="60%"
+          src="https://assets.vogue.ru/photos/5dc55cb3899aac00084fc49f/master/w_1600%2Cc_limit/a87bbb5a054242884cbc6ee8e00da02a.jpg"
+          alt=""
+        />
+      </div>
+      <div className="post">
+        <h2>Influence on world fashion</h2>
+        <p>
+          Coco Chanel has revolutionized the fashion world by offering loose
+          suits and long straight dresses to replace traditional corsets. The
+          couturier has introduced many elements of men's classic fashion into
+          women's clothing. Her simple lines led to the popularity of the boyish
+          build of the female body, the rejection of glamour and excessive
+          luxury in a suit. Coco Chanel clothes also gave women more comfort in
+          everyday life, allowing them to make it more active. Coco made the
+          jersey fabric fashionable, and her branded tweed suits became a symbol
+          of the fashion of the 20s and an eternal classic in the women's
+          wardrobe. Some of the iconic luxury items from Chanel are also quilted
+          bags with long chains, square jackets and pearl necklaces.
+        </p>
+      </div>
     </div>
   );
 };
