5078e2ef4c56f122e0dcceb153778a99a775b660
kavahara <tynaev_97@mail.ru>
2022-04-09 14:59:23 +0600

    navbar-footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,9 +4,38 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <h2>Footer</h2>
+      <div className="footer-one">Chanel</div>
+      <div className="footer-two">
+        <div className=" first sp">
+          <p>Explore chanel.com</p>
+          <ul>
+            <li>Haute Couture</li>
+            <li>Fashion</li>
+          </ul>
+        </div>
+        <div className="second sp">
+          <p>Online services</p>
+          <ul>
+            <li>Payment Methods</li>
+            <li>Shipping Options</li>
+          </ul>
+        </div>
+        <div className="third sp">
+          <p>Boutique services</p>
+          <ul>
+            <li>Store Locator</li>
+            <li>Book an Appointment</li>
+          </ul>
+        </div>
+        <div className="fourth sp">
+          <p>The house of chanel</p>
+          <ul>
+            <li>Careers</li>
+            <li>Legal</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
-
 export default Footer;
